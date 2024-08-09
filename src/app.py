8c8cfb16d49eb15d08a5a334465b7e51b2e52f54
EQ <167933418+ejquast@users.noreply.github.com>
2024-08-09 04:13:27 +0000

    save work, but need pass last 3 fails

--- src/app.py
+++ src/app.py
@@ -15,6 +15,26 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+initial_data = [{
+    "first_name" : "John",
+    "last_name" : "Jackson",
+    "age" : "33",
+    "lucky_numbers" : [7, 13, 22]
+},{
+    "first_name" : "Jane",
+    "last_name" : "Jackson",
+    "age" : "35",
+    "lucky_numbers" : [10, 14, 3]
+},{
+    "first_name" : "Jimmy",
+    "last_name" : "Jackson",
+    "age" : "5",
+    "lucky_numbers" : [1]
+}]
+
+for members_data in initial_data:
+    jackson_family.add_member(members_data)
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -30,13 +50,41 @@ def handle_hello():
 
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    return jsonify(members), 200
 
+def get_all_members(self):
+    return self._members
 
-    return jsonify(response_body), 200
+@app.route('/member/<int:id>', methods=['GET'])
+def find_member(id):
+    member = jackson_family.get_member(id)
+    return jsonify(member), 200
+
+def _generate_id(self):
+        generated_id = self._next_id
+        self._next_id += 1
+        return generated_id
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    ## You have to implement this method
+    ## Append the member to the list of _members
+    member_request_body = request.get_json(force=True)
+    print("Incoming request with the following body", member_request_body)
+    member_request_body = jackson_family.add_member(member_request_body)
+    return jsonify(member_request_body), 200
+
+@app.route('/member', methods=['DELETE'])
+def delete_member(id):
+    ## You have to implement this method
+    ## Loop the list and delete the member with the given id
+    member = jackson_family.get_member(id)
+ 
+    if member:
+        jackson_family.delete_member(id)
+        return jsonify({"message": f"Member deleted successfully: {member}"}), 200
+    else:
+        return jsonify({"error": "Member not found"}), 404
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -21,16 +21,35 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        if "id" not in member:
+            member.update(
+                id=self._generateId()
+            )
+        self._members.append(member)
 
     def delete_member(self, id):
         # fill this method and update the return
-        pass
+        for position, member in enumerate(self._members):
+            if member["id"] == id:
+                self._members.pop(position)
+                return None
+        return None
 
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member["id"] == int(id):
+                return member
+        return None
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
         return self._members
+
+class FamilyMember:
+    def __init__(self, data):
+        self.first_name = data.get("first_name")
+        self.last_name = data["last_name"]
+        self.age = data.get("age", 0)
+        self.lucky_numbers = data.get("lucky_numbers")
+
